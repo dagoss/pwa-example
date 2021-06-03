3af8dbfa2243368d2de6f3f76bd8573bb19e067d
David Goss <goss@frontierscience.org>
2021-06-03 13:22:09 -0400

    misc

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from "react";
-import { SimpleGrid, Box } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Link, Container, Button, Box } from "@chakra-ui/react";
 
 let deferredPrompt;
 
 export default function Home() {
-  const [installable, setInstallable] = useState(false);
+  const [installable, setInstallable] = useState(true);
 
   useEffect(() => {
-    window.addEventListener("beforeinstallprompt", (e) => {
+    window.addEventListener("beforeinstallprompt", e => {
       // Prevent the mini-infobar from appearing on mobile
       e.preventDefault();
       // Stash the event so it can be triggered later.
@@ -16,35 +16,61 @@ export default function Home() {
       setInstallable(true);
     });
 
-    window.addEventListener('appinstalled', () => {
+    window.addEventListener("appinstalled", () => {
       // Log install to analytics
-      console.log('INSTALL: Success');
+      console.log("INSTALL: Success");
     });
   }, []);
 
   const handleInstallClick = () => {
-      // Hide the app provided install promotion
-      setInstallable(false);
-      // Show the install prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-      });
+    // Hide the app provided install promotion
+    setInstallable(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+    });
   };
   return (
-    <SimpleGrid columns={2} spacing={10}>
-      <Box bg="tomato" height="80px">
-              {installable &&
-          <button className="install-button" onClick={handleInstallClick}>
+    <Container centerContent h="100vh" justifyContent="center">
+      <Box
+        mb={4}
+        bg="orange.100"
+        p={4}
+        rounded={4}
+        display="flex"
+        flexDirection={["column", null, "row"]}
+        alignItems="center"
+      >
+        <span>If this app can be installed, you will see a button:</span>{" "}
+        {installable && (
+          <Button
+            colorScheme="orange"
+            ml={[null, null, 2]}
+            mt={[2, null, 0]}
+            onClick={handleInstallClick}
+          >
             Install Now
-          </button>
-        }</Box>
-      <Box bg="tomato" height="80px">Is installable on your device? {installable}</Box>
-    </SimpleGrid>
+          </Button>
+        )}
+      </Box>
+      <div>
+        We could put this in a navbar or alert or something to invite users to
+        install the &quot;app&quot;. (
+        <Link
+          href="https://github.com/dagoss/pwa-example"
+          color="orange.800"
+          fontWeight="bold"
+        >
+          This example
+        </Link>{" "}
+        is using React)
+      </div>
+    </Container>
   );
 }
