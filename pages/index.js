af9ac4e69de824d39e5ad9a1afecbfa6893cfa53
David Goss <goss@frontierscience.org>
2021-06-03 12:15:20 -0400

    added button

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { SimpleGrid, Box } from "@chakra-ui/react";
 
+let deferredPrompt;
+
 export default function Home() {
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener('appinstalled', () => {
+      // Log install to analytics
+      console.log('INSTALL: Success');
+    });
+  }, []);
+
+  const handleInstallClick = () => {
+      // Hide the app provided install promotion
+      setInstallable(false);
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+  };
   return (
     <SimpleGrid columns={2} spacing={10}>
-      <Box bg="tomato" height="80px"></Box>
+      <Box bg="tomato" height="80px">
+              {installable &&
+          <button className="install-button" onClick={handleInstallClick}>
+            INSTALL ME
+          </button>
+        }</Box>
       <Box bg="tomato" height="80px"></Box>
       <Box bg="tomato" height="80px"></Box>
       <Box bg="tomato" height="80px"></Box>
