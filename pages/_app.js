117bdee4d1d020a9d7f3d9eda48fe00a600452a3
David Goss <goss@frontierscience.org>
2021-06-03 12:45:44 -0400

    swapped doc for app

--- pages/_app.js
+++ pages/_app.js
@@ -19,13 +19,13 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
